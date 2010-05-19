37e2daa616cadc9bc6032699792136e75d39994c
William Fagan <willf@commtech-fastcom.com>
2010-05-19 11:10:45 -0500

    Add a couple of examples that showing how to use the flush ioctls.

---
+++ examples/c/flush_tx.c
@@ -0,0 +1,35 @@
+#include <fcntl.h> /* open, O_WRONLY */
+#include <unistd.h> /* close */
+#include <stdio.h> /* perror */
+#include <stdlib.h> /* EXIT_SUCCESS, EXIT_FAILURE */
+#include <sys/ioctl.h> /* ioctl */
+#include <fscc/fscc.h> /* FSCC_FLUSH_TX */
+
+int main(void)
+{
+	int port_fd = 0;
+
+	port_fd = open("/dev/fscc0", O_WRONLY);
+
+	if (port_fd == -1) {
+		perror("open");
+		return EXIT_FAILURE;
+	}
+
+	if (ioctl(port_fd, FSCC_FLUSH_TX) == -1) {
+		perror("FSCC_FLUSH_TX");
+
+		if (close(port_fd) == -1)
+			perror("close");
+			
+		return EXIT_FAILURE;
+	}
+
+	if (close(port_fd) == -1) {
+		perror("close");
+		return EXIT_FAILURE;
+	}	
+	
+	return EXIT_SUCCESS;
+}
+
