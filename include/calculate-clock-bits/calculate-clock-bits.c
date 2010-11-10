4e024410e528f09c3f8ab9b99631f13645b236cf
William Fagan <willf@commtech-fastcom.com>
2010-11-10 17:35:12 -0600

    Add license to a lot of files.

--- include/calculate-clock-bits/calculate-clock-bits.c
+++ include/calculate-clock-bits/calculate-clock-bits.c
@@ -1,3 +1,23 @@
+/*
+	Copyright (C) 2010  Commtech, Inc.
+
+	This file is part of fscc-linux.
+
+	fscc-linux is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	fscc-linux is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with fscc-linux.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -681,6 +701,7 @@ finished:
 		progdata[15]|=0x80;
 		progdata[16]|=0x01;				
 		break;
+
 		
 	case 1750:
 		progdata[11]|=0x08;
