8a603dfc35def27d3005dfd8fd98f80538ffeaac
Matt Schulte <matts@commtech-fastcom.com>
2011-04-22 11:32:56 -0500

    Update install-tools script to use downloads from bitbucket and not google. Also tweaked to make it run without some permission errors.

--- install_tools.sh
+++ install_tools.sh
@@ -3,24 +3,26 @@
 # Downloads and installs the various tools related to the FSCC (command line,
 # graphical settings editor and python libraries).
 
-rm -r tools
+sudo rm -r tools
 mkdir tools
 cd tools
 
-wget http://pyfscc.googlecode.com/files/pyfscc.tar.gz
-wget http://fscc-cli.googlecode.com/files/fscc-cli.tar.gz
-# wget http://fscc-gui.googlecode.com/files/fscc-gui.tar.gz
+wget http://hg.commtech-fastcom.com/pyfscc/get/tip.tar.gz
+mv tip.tar.gz pyfscc.tar.gz
+tar xzf pyfscc.tar.gz
+wget http://hg.commtech-fastcom.com/fscc-cli/get/tip.tar.gz
+mv tip.tar.gz fscc-cli.tar.gz
+tar xzf fscc-cli.tar.gz
+#wget http://hg.commtech-fastcom.com/fscc-gui/get/tip.tar.gz
+#mv tip.tar.gz fscc-gui.tar.gz
+#tar xzf fscc-gui.tar.gz
 
-tar xf pyfscc*.tar.gz
-tar xf fscc-cli*.tar.gz
-tar xf fscc-gui*.tar.gz
-
-cd pyfscc*
-python setup.py install
+cd fastcom-pyfscc*
+sudo python setup.py install
 cd ..
 
-cd fscc-cli*
-python setup.py install
+cd fastcom-fscc-cli*
+sudo python setup.py install
 cd ..
 
 # cd fscc-gui*
@@ -28,4 +30,3 @@ cd ..
 # cd ..
 
 cd ..
-rm -r tools/
