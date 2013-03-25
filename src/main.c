227e6d66a21029b846a8bea35cbf0903230db433
William Fagan <willf@commtech-fastcom.com>
2013-03-25 13:14:13 -0500

    Bump version and update ChangeLog for release.

--- src/main.c
+++ src/main.c
@@ -428,7 +428,7 @@ static void __exit fscc_exit(void)
 MODULE_DEVICE_TABLE(pci, fscc_id_table);
 
 MODULE_LICENSE("GPL");
-MODULE_VERSION("2.2.1");
+MODULE_VERSION("2.2.2");
 MODULE_AUTHOR("William Fagan <willf@commtech-fastcom.com>");
 
 MODULE_DESCRIPTION("Driver for the FSCC series of cards from Commtech, Inc.");
