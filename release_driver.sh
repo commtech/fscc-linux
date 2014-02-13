740215d29e1c1d65a1decfdd58e5f2c48d87bc02
William Fagan <willf@commtech-fastcom.com>
2014-02-13 12:15:27 -0600

    Add release driver script.

---
+++ release_driver.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+VER=2.5.0
+
+git clone --recursive git@github.com:commtech/fscc-linux.git fscc-linux-$VER
+cd fscc-linux-$VER
+rm -rf .git*
+cd -
+tar -czf fscc-linux-$VER.tar.gz fscc-linux-$VER/
+rm -rf fscc-linux-$VER
