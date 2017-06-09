0028c3901e83caf68d8aea97463f23ff030d111f
lunruh <landonu@commtech-fastcom.com>
2017-06-09 09:20:14 -0500

    Create main.c

--- src/main.c
+++ src/main.c
@@ -483,7 +483,7 @@ static void __exit fscc_exit(void)
 MODULE_DEVICE_TABLE(pci, fscc_id_table);
 
 MODULE_LICENSE("GPL");
-MODULE_VERSION("2.5.1");
+MODULE_VERSION("2.5.2");
 MODULE_AUTHOR("William Fagan <willf@commtech-fastcom.com>");
 
 MODULE_DESCRIPTION("Driver for the FSCC series of cards from Commtech, Inc.");
